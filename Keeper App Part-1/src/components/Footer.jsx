390816e0636b5ca5f8d7db94744a194eaffcfa6b
Shaik Firdos <fidduraza9876@gmail.com>
2024-01-18 21:05:50 +0530

    Add files via upload

---
+++ Keeper App Part-1/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const date = new Date();
+const currentYear = date.getFullYear();
+
+function Footer(){
+    return (
+      <footer>
+        <p>copyright © {currentYear}</p>
+      </footer>
+    );
+}
+
+export default Footer;
